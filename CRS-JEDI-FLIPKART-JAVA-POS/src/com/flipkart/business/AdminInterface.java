8e30c8d45f1c87f430d52d90266ddd4c113fc6eb
utsav000 <utsavgupta8770@gmail.com>
2024-05-23 00:03:04 +0530

    Admin

--- CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/business/AdminInterface.java
+++ CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/business/AdminInterface.java
@@ -3,9 +3,24 @@
  */
 package com.flipkart.business;
 
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Professor;
+
 /**
  * 
  */
 public interface AdminInterface {
+	public boolean addProfessor(String userID, String name, String gender, String role, String password, String address);
 
+    public boolean assignCourse(int professorId, String courseCode);
+
+    public boolean approveStudent(int studentId);
+
+    public boolean updateDetails();
+
+    public boolean addCourse(Course newCourse);
+
+    public boolean removeCourse(String courseCode);
+
+    public boolean generateReportCard();
 }
--- CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/business/AdminOperation.java
+++ CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/business/AdminOperation.java
@@ -3,9 +3,71 @@
  */
 package com.flipkart.business;
 
+import java.util.Vector;
+
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Professor;
+import com.flipkart.bean.Student;
+
 /**
  * 
  */
-public class AdminOperation {
+public class AdminOperation implements AdminInterface {
+	public Vector <Professor> Professorslist = new Vector <Professor>();
+
+	public boolean addProfessor(String userID, String name, String gender, String role, String password, String address) {
+		// TODO Auto-generated method stub
+		Professor newpf = new Professor(userID, name, gender, role, password, address); 
+		Professorslist.add(newpf);
+		System.out.println("New Professor added");
+
+		return false;
+	}
+	
+	public int getStudentlist() {
+		// TODO Auto-generated method stub
+		for (Professor professor: Professorslist) {
+			System.out.println("Details of the customer are: " + professor.getName() + " "
+					+ professor.getDepartment() + " " + professor.getDepartment());
+		}
+		return 0;
+	}
+
+	@Override
+	public boolean assignCourse(int professorId, String courseCode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean approveStudent(int studentId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateDetails() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addCourse(Course newCourse) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeCourse(String courseCode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean generateReportCard() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 
 }
