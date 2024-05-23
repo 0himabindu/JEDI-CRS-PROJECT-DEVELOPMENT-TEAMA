096ea1bc0ba7236e944fc74c862de6dd8e4953b6
aish0749 <aishwaryasaxena57@gmail.com>
2024-05-23 10:11:18 +0530

    update bean

---
+++ CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/bean/SemesterRegistration.java
@@ -0,0 +1,27 @@
+package com.flipkart.bean;
+
+public class SemesterRegistration {
+    private int studentId;
+    private int semester;
+
+    SemesterRegistration(int studentId, int semester) {
+        this.studentId = studentId;
+        this.semester = semester;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+}
